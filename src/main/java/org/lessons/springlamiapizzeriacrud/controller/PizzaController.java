69114eef1c2325470427e300609b832799465557
DavideSias <davidesias77@gmail.com>
2023-06-28 15:54:24 +0200

    add update and delete crud

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,27 +37,66 @@ public class PizzaController {
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Integer pizzaId, Model model) {
-        Optional<Pizza> result = pizzaRepository.findById(pizzaId);
+        /*Optional<Pizza> result = pizzaRepository.findById(pizzaId);
         if (result.isPresent()) {
             model.addAttribute("pizza", result.get());
             return "/pizza/pizzaDetail";
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + pizzaId + " not found");
-        }
+        }*/
+        Pizza pizza = getPizzaById(pizzaId);
+        model.addAttribute("pizza", pizza);
+        return "/pizza/pizzaDetail";
     }
 
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
-        return "/pizza/create";
+        return "/pizza/edit";
     }
 
     @PostMapping("/create")
-    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult) {
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, RedirectAttributes redirAttrs) {
         if (bindingResult.hasErrors()) {
-            return "/pizza/create";
+            return "/pizza/edit";
         }
+        redirAttrs.addFlashAttribute("message", "New Pizza added");
         pizzaRepository.save(formPizza);
         return "redirect:/pizza";
+    }
+
+    @GetMapping("edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Pizza result = getPizzaById(id);
+        model.addAttribute("pizza", result);
+        return "/pizza/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        Pizza pizzaEdit = getPizzaById(id);
+        if (bindingResult.hasErrors()) {
+            return "/pizza/edit";
+        }
+        formPizza.setId(formPizza.getId());
+        pizzaRepository.save(formPizza);
+        redirectAttributes.addFlashAttribute("message", "Pizza updated");
+        return "redirect:/pizza";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        Pizza pizzaToDelete = getPizzaById(id);
+        pizzaRepository.delete(pizzaToDelete);
+        redirectAttributes.addFlashAttribute("message", "Pizza deleted");
+        return "redirect:/pizza";
+    }
+
+    private Pizza getPizzaById(Integer id) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id" + id + " not found");
+        }
+        return result.get();
     }
 }
--- src/main/java/org/lessons/springlamiapizzeriacrud/model/Pizza.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/model/Pizza.java
@@ -3,6 +3,7 @@ package org.lessons.springlamiapizzeriacrud.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
